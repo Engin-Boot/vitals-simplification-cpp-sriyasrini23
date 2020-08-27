d6525364eca24c3825cbb13c945e2b58225fd3b3
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-08-27 10:25:22 +0400

    Update checker.cpp
    abstraction level

--- checker.cpp
+++ checker.cpp
@@ -2,9 +2,10 @@
 
 bool is_in_limits (float value, float lower_limit, float upper_limit)
 {
-  return (value<upper_limit && value>lower_limit);
+  return (value<=upper_limit && value>=lower_limit);
 }
 
+/*
 bool bpm_is_Ok(float bpm)
 {
   return is_in_limits(bpm, 70, 150);
@@ -19,13 +20,22 @@ bool respRate_is_Ok(float respRate)
 {
     return is_in_limits(respRate, 30, 95);
 }
+*/
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
 {
-    return( bpm_is_Ok(bpm) && spo2_is_Ok(spo2) && respRate_is_Ok(respRate) );  
+    return( is_in_limits(bpm, 70, 150) && is_in_limits(spo2, 90, 100) && is_in_limits(respRate, 30, 95) );  
 }
 
-int main() {
+int main() 
+{
+  assert (is_in_limits(20,10,30) == true);
+  assert (is_in_limits(10,20,30) == false);
+  assert (is_in_limits(10,10,30) == true);
+  assert (is_in_limits(30,10,30) == true);
+  
+  /*
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
+  */
 }
