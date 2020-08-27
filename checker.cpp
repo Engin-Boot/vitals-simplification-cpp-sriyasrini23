7941a67ffb0169e2ffb6d5c6014feedf6f0ff86e
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-08-27 09:28:13 +0400

    Update checker.cpp
    Split functionality

--- checker.cpp
+++ checker.cpp
@@ -1,14 +1,24 @@
 #include <assert.h>
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
+
+bool bpm_is_Ok(float bpm)
+{
+  return !(bpm < 70 || bpm > 150);
+}
+
+bool spo2_is_Ok(float spo2)
+{
+  return !(spo2 < 90);
+}
+
+bool respRate_is_Ok(float respRate)
+{
+  return !(respRate < 30 || respRate > 95);
+}
+
+bool vitalsAreOk(float bpm, float spo2, float respRate) 
+{
+    return( bpm_is_Ok(bpm) && spo2_is_OK(spo2) && respRate_is_OK(respRate) );  
 }
 
 int main() {
