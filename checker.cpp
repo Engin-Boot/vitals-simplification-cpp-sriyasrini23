b1a5b6e076eec21bc47d58ba4f4b3115d8a776c4
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-08-27 10:14:53 +0400

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,22 +2,22 @@
 
 bool is_in_limits (float value, float lower_limit, float upper_limit)
 {
-  return ( value<upper_limit && value>lower_limit);
+  return (value<upper_limit && value>lower_limit);
 }
 
 bool bpm_is_Ok(float bpm)
 {
-  return is_in_limit(bpm, 70, 150);
+  return is_in_limits(bpm, 70, 150);
 }
 
 bool spo2_is_Ok(float spo2)
 {
-  return is_in_limit(spo2, 90, 100);
+  return is_in_limits(spo2, 90, 100);
 }
 
 bool respRate_is_Ok(float respRate)
 {
-    return is_in_limit(bpm, 30, 95);
+    return is_in_limits(respRate, 30, 95);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
