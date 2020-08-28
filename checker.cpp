6c25b689d2c2a3566086fce0f19ba02d148185f0
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-08-28 10:18:32 +0400

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,24 +1,41 @@
 #include <assert.h>
+#include <iostream>
+
+
+void raiseSoundAlert (const char * vitalName, const char* level)
+{
+  std::cout << "Sound: " << vitalName << " is " << level << std::endl;
+}
+
+bool level_is_ok (const char* name, float value, float lower_limit, float upper_limit)
+{
+  if (value<lower_limit)
+  {
+   raiseSoundAlert (name, "LOW");
+    return false;
+  }
+  else if (value>upper_limit)
+  {
+    raiseSoundAlert (name, "HIGH");
+    return false;
+  }
+  else 
+    return true;
+    
+}
 
 bool is_in_limits (float value, float lower_limit, float upper_limit)
-{
+{ 
   return (value<=upper_limit && value>=lower_limit);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) 
 {
-    return( is_in_limits(bpm, 70, 150) && is_in_limits(spo2, 90, 100) && is_in_limits(respRate, 30, 95) );  
+    return( level_is_ok("bpm",bpm, 70, 150) && level_is_ok("spo2",spo2, 90, 100) && level_is_ok("respRape",respRate, 30, 95) );  
 }
 
 int main() 
 {
-  assert (is_in_limits(20,10,30) == true);
-  
-  assert (is_in_limits(10,20,30) == false);
-  assert (is_in_limits(40,20,30) == false);
-  
-  assert (is_in_limits(10,10,30) == true);
-  assert (is_in_limits(30,10,30) == true);
   
   
   assert(vitalsAreOk(80, 95, 60) == true);
